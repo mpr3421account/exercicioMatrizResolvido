2cceed29e0c93d17092da155f5c9f150ac7462ee
Marcos Paulo Ribeiro <31698863+mpr3421account@users.noreply.github.com>
2022-12-08 14:29:25 -0300

    Implementado Matriz com for

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Globalization;
+
+int n = int.Parse(Console.ReadLine());
+
+int[,] mat = new int[n,n];
+for (int i = 0;i< n; i++)
+{
+    string[] values = Console.ReadLine().Split(' ');
+
+    for(int j = 0; j < n; j++)
+    {
+        mat[i,j] = int.Parse(values[j]);
+    }
+}
+
